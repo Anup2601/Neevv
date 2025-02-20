af5f66c2f06d00778067d98662c78c3c7a0fad8b
Anup Mishra <anupm0873@gmail.com>
2025-02-20 06:08:03 +0530

    connect to database

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,14 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
 const app=express();
 
 import authRoutes from "./Routes/auth.route.js"
-
+// Load environment variables
+dotenv.config({ path: "../.env" });
+// Middleware and Routes
 app.use("/api/auth", authRoutes);
 
 app.get("/",(req,res)=>{
     res.send("Working");
 })
+// Ensure PORT is properly defined
+const PORT=process.env.PORT || 5000;
+console.log("Loaded PORT:", process.env.PORT);
 
-app.listen(8080,()=>{
-    console.log("listing to port");
-})
+
+app.listen(PORT,()=>{
+    console.log("listing to port: "+ PORT);
+});
