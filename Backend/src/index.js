f24b66e3b0f00ef2cb8d5cc25d64053f4d14f811
Anup Mishra <anupm0873@gmail.com>
2025-02-23 11:29:56 +0530

    basic frontend setup

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,15 +4,23 @@ import cookieParser from "cookie-parser"
 import authRoutes from "./Routes/auth.route.js"
 import { connectDB } from "./lib/db.js";
 import messageRoute from "./Routes/message.route.js"
+import cors from "cors"
+
 const app=express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials:true,
+}))
 // Load environment variables
 dotenv.config({ path: "../.env" });
 // Middleware and Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/message",messageRoute);
+
+
 
 app.get("/",(req,res)=>{
     res.send("Working");
