dae7b19499c2952dfd9edc530a5969dd436e6a09
Anup Mishra <anupm0873@gmail.com>
2025-03-09 07:40:24 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,9 +35,9 @@ const PORT=process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"frontend","build","index.html"));
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
     })
 }
 
